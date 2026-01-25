6ffaddf97b00c166aa3c8a5b635093f67ca7e60b
Olaren <git@olaren.dev>
2026-01-25 11:25:24 -0500

    fix: osmnx already does caching

--- src/maptoposter/fetching.py
+++ src/maptoposter/fetching.py
@@ -1,91 +1,44 @@
-from typing import Callable, Any, Tuple
-from geopandas import GeoDataFrame
-from networkx import MultiDiGraph
-from maptoposter import cache
+from typing import Tuple
 from maptoposter.poster import PosterData
 import osmnx
 
 
 def fetch_point(search_term: str) -> Tuple[float, float]:
-    return _cached_fetch(f"location_{search_term}", lambda: osmnx.geocode(search_term))
+    return osmnx.geocode(search_term)
 
 
 def fetch_data(point: Tuple[float, float], radius: int) -> PosterData:
     print("Retrieving parks/green spaces...")
-    parks = _fetch_features(
+    parks = osmnx.features_from_point(
         point,
-        radius,
         {"leisure": "park", "landuse": ["grass", "cemetery"], "natural": "wood"},
-        "parks",
+        radius,
     )
 
     print("Retrieving water features...")
-    water = _fetch_features(
-        point,
-        radius,
-        {"natural": ["water", "bay"], "waterway": "riverbank"},
-        "water",
+    water = osmnx.features_from_point(
+        point, {"natural": ["water", "bay"], "waterway": "riverbank"}, radius
     )
 
     print("Retrieving roads...")
-    roads = _fetch_roads(point, radius)
+    roads = osmnx.graph_from_point(
+        point,
+        dist=radius,
+        dist_type="bbox",
+        network_type="all",
+        truncate_by_edge=True,
+    )
 
     print("Retrieving train network...")
-    trains = _fetch_features(point, radius, {"railway": "rail"}, "rail")
+    trains = osmnx.features_from_point(point, {"railway": "rail"}, radius)
 
     print("Retrieving tram network...")
-    trams = _fetch_features(point, radius, {"railway": "tram"}, "tram")
+    trams = osmnx.features_from_point(point, {"railway": "tram"}, radius)
 
     print("Retrieving light rail network...")
-    light_rails = _fetch_features(
-        point, radius, {"railway": "light_rail"}, "light_rail"
-    )
+    light_rails = osmnx.features_from_point(point, {"railway": "light_rail"}, radius)
 
     print("Retrieving subway network...")
-    subways = _fetch_features(point, radius, {"railway": "subway"}, "subway")
+    subways = osmnx.features_from_point(point, {"railway": "subway"}, radius)
 
     return PosterData(parks, water, roads, subways, trams, light_rails, trains)
-
-
-def _fetch_roads(point: Tuple[float, float], radius: int) -> MultiDiGraph:
-    lat, lon = point
-    key = f"roads_{lat}_{lon}_{radius}"
-    return _cached_fetch(
-        key,
-        lambda: osmnx.graph_from_point(
-            point,
-            dist=radius,
-            dist_type="bbox",
-            network_type="all",
-            truncate_by_edge=True,
-        ),
-    )
-
-
-def _fetch_features(
-    point: Tuple[float, float],
-    radius: int,
-    tags: dict[str, bool | str | list[str]],
-    name: str,
-) -> GeoDataFrame | None:
-    lat, lon = point
-    key = f"{name}_{lat}_{lon}_{radius}"
-    return _cached_fetch(key, lambda: osmnx.features_from_point(point, tags, radius))
-
-
-def _cached_fetch(key: str, fetch_fn: Callable[[], Any]) -> Any:
-    cached = cache.get(key)
-    if cached is not None:
-        print(f"✓ Using cached {key}")
-        return cached
-
-    try:
-        result = fetch_fn()
-        cache.set(key, result)
-        return result
-    except cache.CacheError as e:
-        print(e)
-        return None
-    except Exception as e:
-        print(f"OSMnx error while fetching {key}: {e}")
-        return None
